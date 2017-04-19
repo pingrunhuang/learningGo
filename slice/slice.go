cd8c0a6b2fe5e270ee5040c70bf983ad6a0301bf
Huang Runping <pingrunhuang@gmail.com>
2017-04-19 16:15:00 +0800

    slice

---
+++ slice/slice.go
@@ -0,0 +1,19 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	result := make([][]uint8, dy)
+	for i := 0; i < dy; i++{
+		result[i] = make([]uint8, dx)
+		for j := 0; j < dy; j++ {
+			result[i][j] = uint8((i+j)/2)
+		}
+	}
+
+    return result
+}
+
+func main() {
+	pic.Show(Pic)
+}
